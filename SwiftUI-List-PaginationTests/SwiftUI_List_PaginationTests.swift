e7712acc870aaa5d65bb14812bfa57addabf30f9
mcz9mm <k.m.swim411@gmail.com>
2019-06-16 17:23:23 +0900

    Initial Commit

---
+++ SwiftUI-List-PaginationTests/SwiftUI_List_PaginationTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUI_List_PaginationTests.swift
+//  SwiftUI-List-PaginationTests
+//
+//  Created by MataraiKaoru on 2019/06/16.
+//  Copyright © 2019 MataraiKaoru. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftUI_List_Pagination
+
+class SwiftUI_List_PaginationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
